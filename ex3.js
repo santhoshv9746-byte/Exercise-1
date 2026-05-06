ccfc7cc50e441e40e1b91c6c372dd5f7707fa376
Agnix <santhoshv9746@gmail.com>
2026-05-07 00:07:51 +0100

    Update ex3.js
    ex.3

--- ex3.js
+++ ex3.js
@@ -1,7 +1,9 @@
 let listEuler1 = (a, b, l) => {
+
     let sum = 0;
 
     for (let x of l) {
+
         if (x % a === 0 || x % b === 0) {
             sum += x;
         }
@@ -11,11 +13,15 @@ let listEuler1 = (a, b, l) => {
 }
 
 let listEuler2 = (a, l) => {
+
     let sum = 0;
 
     for (let x of l) {
+
         for (let m of a) {
+
             if (x % m === 0) {
+
                 sum += x;
                 break;
             }
@@ -26,42 +32,58 @@ let listEuler2 = (a, l) => {
 }
 
 let listEuler3 = (a, l) => {
+
     return l
         .filter(x => a.some(m => x % m === 0))
         .reduce((acc, val) => acc + val, 0);
 }
 
 let eulerlist = () => {
-    let a = parseInt(document.getElementById('a1').value);
-    let b = parseInt(document.getElementById('b1').value);
 
-    let l = document.getElementById('l').value
-        .split(" ")
-        .map(x => parseInt(x));
+    let a =
+        parseInt(
+            document.getElementById('a1').value
+        );
+
+    let b =
+        parseInt(
+            document.getElementById('b1').value
+        );
+
+    let l =
+        document.getElementById('l').value
+            .split(" ")
+            .map(x => parseInt(x));
 
     alert(listEuler1(a, b, l));
 }
 
 let euler2Lists = () => {
-    let a = document.getElementById('aList').value
-        .split(" ")
-        .map(x => parseInt(x));
 
-    let l = document.getElementById('mList').value
-        .split(" ")
-        .map(x => parseInt(x));
+    let a =
+        document.getElementById('aList').value
+            .split(" ")
+            .map(x => parseInt(x));
+
+    let l =
+        document.getElementById('mList').value
+            .split(" ")
+            .map(x => parseInt(x));
 
     alert(listEuler2(a, l));
 }
 
 let euler2Lists1 = () => {
-    let a = document.getElementById('aList').value
-        .split(" ")
-        .map(x => parseInt(x));
 
-    let l = document.getElementById('mList').value
-        .split(" ")
-        .map(x => parseInt(x));
+    let a =
+        document.getElementById('aList').value
+            .split(" ")
+            .map(x => parseInt(x));
+
+    let l =
+        document.getElementById('mList').value
+            .split(" ")
+            .map(x => parseInt(x));
 
     alert(listEuler3(a, l));
 }
