85a75e4b20864d1fde0062ee9c2a2fc440d7d163
Agnix <santhoshv9746@gmail.com>
2026-05-06 23:30:44 +0100

    Update ex3.js
    let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
    let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
    let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
    
    let eulerlist=()=>
    {
        a=2//can un-hardcode
        b=3
        l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
        //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
    }
    let euler2Lists=()=>
    {
        a=[2,3]//can un-hardcode
        l=[1,2,3,4,5,6,7,9,10,10,10]
        //call listEuler2 and alert.
    }
    
    let euler2Lists1=()=>
    {
        a=[2,3,5]//can un-hardcode
        l=[1,2,3,4,5,6,7,9,10,10,10]
        //call listEuler3 and alert.
    }

--- ex3.js
+++ ex3.js
@@ -1,24 +1,62 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
-let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
-let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
+// Sum of numbers in l that are multiples of a OR b
+let listEuler1 = (a, b, l) => {
+  let sum = 0;
 
-let eulerlist=()=>
-{
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]  //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
-    //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
-}
-let euler2Lists=()=>
-{
-    a=[2,3]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler2 and alert.
-}
+  for (let x of l) {
+    if (x % a === 0 || x % b === 0) {
+      sum += x;
+    }
+  }
 
-let euler2Lists1=()=>
-{
-    a=[2,3,5]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler3 and alert.
-}
+  return sum;
+};
+
+// Sum of numbers in l that are multiples of ANY value in list a
+let listEuler2 = (a, l) => {
+  let sum = 0;
+
+  for (let x of l) {
+    for (let m of a) {
+      if (x % m === 0) {
+        sum += x;
+        break; // avoid double counting
+      }
+    }
+  }
+
+  return sum;
+};
+
+// Same as listEuler2 (more functional style)
+let listEuler3 = (a, l) => {
+  return l
+    .filter(x => a.some(m => x % m === 0))
+    .reduce((acc, val) => acc + val, 0);
+};
+
+// ---- CALLING FUNCTIONS ----
+
+let eulerlist = () => {
+  let a = 2;
+  let b = 3;
+  let l = [1,2,3,4,5,6,7,9,10,10,10];
+
+  let result = listEuler1(a, b, l);
+  alert("Sum (a or b): " + result);
+};
+
+let euler2Lists = () => {
+  let a = [2,3];
+  let l = [1,2,3,4,5,6,7,9,10,10,10];
+
+  let result = listEuler2(a, l);
+  alert("Sum (list a): " + result);
+};
+
+let euler2Lists1 = () => {
+  let a = [2,3,5];
+  let l = [1,2,3,4,5,6,7,9,10,10,10];
+
+  let result = listEuler3(a, l);
+  alert("Sum (functional): " + result);
+};
