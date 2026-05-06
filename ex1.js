e62441c95547a2b82741a21ef29e7e6ede8361bc
Agnix <santhoshv9746@gmail.com>
2026-05-07 00:06:13 +0100

    Update ex1.js
    ex1

--- ex1.js
+++ ex1.js
@@ -10,7 +10,11 @@
 let maxWeight = 15;
 
 let check = () => {
-    let weight = parseFloat(document.getElementById('weight').value);
+
+    let weight =
+        parseFloat(
+            document.getElementById('weight').value
+        );
 
     if (isNaN(weight)) {
         alert("Please enter a valid weight");
@@ -18,29 +22,46 @@ let check = () => {
     }
 
     if (weight > maxWeight) {
+
         alert("Overweight baggage!");
 
         document.getElementById('removed').style.display = 'block';
+
         document.getElementById('remB').style.display = 'block';
+
     }
     else {
+
         alert("Weight is OK");
+
     }
 }
 
 let removeWeight = () => {
-    let weight = parseFloat(document.getElementById('weight').value);
-    let removed = parseFloat(document.getElementById('removed').value);
+
+    let weight =
+        parseFloat(
+            document.getElementById('weight').value
+        );
+
+    let removed =
+        parseFloat(
+            document.getElementById('removed').value
+        );
 
     let newWeight = weight - removed;
 
     document.getElementById('weight').value = newWeight;
 
     if (newWeight <= maxWeight) {
+
         alert("Now OK to proceed");
+
     }
     else {
+
         alert("Still overweight");
+
     }
 }
 </script>
