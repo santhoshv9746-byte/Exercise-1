a3d29758e070b43cec242c59d1a59d3c1478e3aa
Agnix <santhoshv9746@gmail.com>
2026-05-06 23:32:30 +0100

    Update ex2.js
    //redefine these - ex1 should output the solution to project Euler Q1
    //https://projecteuler.net/problem=1
    //ex2 should output the sum of all multiples of a or b less than n
    
    let euler1=()=>alert();
    
    let eulerCustom=()=>alert()

--- ex2.js
+++ ex2.js
@@ -1,7 +1,46 @@
-//redefine these - ex1 should output the solution to project Euler Q1
-//https://projecteuler.net/problem=1
-//ex2 should output the sum of all multiples of a or b less than n
+<input type="number" id="n" placeholder="Enter n">
+<button onclick="euler1()">Euler Q1 (n = 1000)</button>
 
-let euler1=()=>alert();
+<br><br>
 
-let eulerCustom=()=>alert()
+<input type="number" id="a" placeholder="Enter a">
+<input type="number" id="b" placeholder="Enter b">
+<input type="number" id="limit" placeholder="Enter n">
+<button onclick="eulerCustom()">Custom</button>
+
+<script>
+// Project Euler Q1: sum of multiples of 3 or 5 below 1000
+let euler1 = () => {
+  let sum = 0;
+
+  for (let i = 1; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  alert("Euler Q1 Answer: " + sum); // should be 233168
+};
+
+// Custom version: sum of multiples of a or b below n
+let eulerCustom = () => {
+  let a = parseInt(document.getElementById('a').value);
+  let b = parseInt(document.getElementById('b').value);
+  let n = parseInt(document.getElementById('limit').value);
+
+  if (isNaN(a) || isNaN(b) || isNaN(n)) {
+    alert("Please enter valid numbers");
+    return;
+  }
+
+  let sum = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (i % a === 0 || i % b === 0) {
+      sum += i;
+    }
+  }
+
+  alert("Sum: " + sum);
+};
+</script>
