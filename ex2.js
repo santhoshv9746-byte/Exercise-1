c79c8eaf226f121842d1c672a68abc5e0fda8237
Agnix <santhoshv9746@gmail.com>
2026-05-07 00:07:15 +0100

    Update ex2.js
    ex2

--- ex2.js
+++ ex2.js
@@ -11,9 +11,11 @@
 <script>
 // Project Euler Q1: sum of multiples of 3 or 5 below 1000
 let euler1 = () => {
+
     let sum = 0;
 
     for (let i = 1; i < 1000; i++) {
+
         if (i % 3 === 0 || i % 5 === 0) {
             sum += i;
         }
@@ -23,13 +25,26 @@ let euler1 = () => {
 }
 
 let eulerCustom = () => {
-    let a = parseInt(document.getElementById('a').value);
-    let b = parseInt(document.getElementById('b').value);
-    let n = parseInt(document.getElementById('n').value);
+
+    let a =
+        parseInt(
+            document.getElementById('a').value
+        );
+
+    let b =
+        parseInt(
+            document.getElementById('b').value
+        );
+
+    let n =
+        parseInt(
+            document.getElementById('n').value
+        );
 
     let sum = 0;
 
     for (let i = 1; i < n; i++) {
+
         if (i % a === 0 || i % b === 0) {
             sum += i;
         }
